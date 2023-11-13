0a2bb8c2d21dd5fcd1a310e76516fe733371c84f
npham49 <brian.d.pham@gov.bc.ca>
2023-11-13 13:08:22 -0800

    removed redundant code

--- me-fi/components/journal/History.tsx
+++ me-fi/components/journal/History.tsx
@@ -1,9 +1,4 @@
-import { StyleSheet } from "react-native";
-import {
-  View,
-  Card,
-  Text,
-} from "react-native-ui-lib";
+import { View, Card, Text } from "react-native-ui-lib";
 
 const cardImage = require("../../assets/content.png");
 
@@ -14,7 +9,7 @@ const data: any[] = [
     date: "Feb 1st",
     description: "Rough terrain quick trip",
     location: "Mänttä-Vilppula",
-    group: "Happy Bikers"
+    group: "Happy Bikers",
   },
   {
     title: "Trip 2",
@@ -22,7 +17,7 @@ const data: any[] = [
     date: "Feb 15th",
     description: "Quick morning trip at",
     location: "Helsinki",
-    group: "Adventure Seekers"
+    group: "Adventure Seekers",
   },
   {
     title: "Trip 3",
@@ -30,14 +25,14 @@ const data: any[] = [
     date: "Feb 20th",
     description: "Quick trip",
     location: "Oulu",
-    group: "Happy Bikers"
+    group: "Happy Bikers",
   },
 ];
 
 const History = () => {
   return (
     <>
-      <View centerH flex marginV-10 >
+      <View centerH flex marginV-10>
         {data.map((item: any, index) => {
           return (
             <Card
@@ -79,72 +74,28 @@ const History = () => {
                 <Text style={{ marginBottom: 10, fontSize: 15, color: "grey" }}>
                   {item.description}
                 </Text>
-                <Text style={{ marginBottom: 10, fontSize: 15, color: "black" }}>
+                <Text
+                  style={{ marginBottom: 10, fontSize: 15, color: "black" }}
+                >
                   Biking trip with {item.group}
                 </Text>
               </View>
             </Card>
           );
         })}
-                        <Text style={{ marginBottom: 10, fontSize: 15, color: "black", fontStyle:"italic" }}>
-                 End of History
-                </Text>
+        <Text
+          style={{
+            marginBottom: 10,
+            fontSize: 15,
+            color: "black",
+            fontStyle: "italic",
+          }}
+        >
+          End of History
+        </Text>
       </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
-  },
-  modalView: {
-    width: "80%",
-    height: "60%",
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    alignItems: "flex-start",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    position: "absolute",
-    right: -10,
-    top: -10,
-    borderRadius: 40,
-    paddingHorizontal: 13,
-    paddingVertical: 10,
-    elevation: 2,
-  },
-  buttonClose: {
-    backgroundColor: "black",
-    zIndex: 1,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 15,
-  },
-  modalTextSub: {
-    marginBottom: 15,
-    fontSize: 15,
-    fontWeight: "600",
-  },
-});
 
 export default History;
